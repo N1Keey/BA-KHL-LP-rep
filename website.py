0e3dcb7dd6edc81d9e5636fc64c0a6b0dc7dd85e
Nico <>
2020-10-06 12:26:50 +0200

    updated variablenames
    next !Register über Owner
    sondern
    Get methode raus um Login funktionsfähig zu machen

--- website.py
+++ website.py
@@ -1,9 +1,6 @@
 from flask import Flask, request,render_template
 import database
 
-user="Owner"
-password="Ownerpassword"
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
@@ -12,30 +9,30 @@ def login():
     if request.method=='POST':
         email=rform['Email']
         password=rform['Passwort']
-        login_val=database.accLogin(email,password)
-        return render_template('Login.j2',login_val=login_val)
+        login_message=database.accLogin(email,password)
+        return render_template('Login.j2',login_message=login_message)
     return render_template('Login.j2')
 
 
-# pw_best=Passwortbestätigung
+# pw_validation=Passwortbestätigung
 @app.route('/registrieren',methods=['GET','POST'])
 def registrieren():
     rform=request.form
     if request.method=='POST':
         email=rform['Email']
         password=rform['Passwort']
-        pw_best=rform['pw_best']
+        pw_validation=rform['pw_validation']
         dbemails=database.readsql('accounts')
         if email in dbemails:
-            pw_val='Email existiert schon!'
-            return render_template('Login.j2', pw_val=pw_val) 
-        if password == pw_best:
+            regist_message='Email existiert schon!'
+            return render_template('Login.j2', regist_message=regist_message) 
+        if password == pw_validation:
             database.accRegister(email,password)
-            pw_val='Erfolgreich registriert!'
-            return render_template('Login.j2', pw_val=pw_val)       
+            regist_message='Erfolgreich registriert!'
+            return render_template('Login.j2', regist_message=regist_message)       
         else:
-            pw_val='Passwörter stimmen nicht überein!'
-            return render_template('Registrieren.j2', pw_val=pw_val)
+            regist_message='Passwörter stimmen nicht überein!'
+            return render_template('Registrieren.j2', regist_message=regist_message)
     return render_template('Registrieren.j2')
 
 @app.route('/index',methods=['GET','POST'])
@@ -54,7 +51,6 @@ def lernen():
 @app.route('/prüfen',methods=['GET','POST'])
 def prüfen():
     return render_template('Prüfen.j2')
-
 
 if __name__=='__main__':
     app.run()
