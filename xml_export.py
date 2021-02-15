6cbee8864705d4aad95c098d8fa089a6d62447e9
Nico <>
2021-02-15 21:12:52 +0100

    Doku in xml verbessert

--- xml_export.py
+++ xml_export.py
@@ -4,6 +4,12 @@ from datetime import datetime
 #build tree
 
 def create_quiz(headerelements, fragendicts):
+    """baut fragen4xml aus fragendicts
+    Parameter: headerelements = List[String], fragendicts = List[Dict]
+        Dict = {'Krankheit':krankheit, 'Umstände':{'Ursachen':[],'Symptome':[],
+                'Komplikationen':[], 'Diagnostiken':[], 'Therapien':[]},'Fragentyp':fragentyp}
+    Return: quiz = xmltree
+    """
     quiz=ET.Element('quiz')
     for textelement in headerelements:
         quiz.append(create_question_header(textelement))
@@ -12,21 +18,38 @@ def create_quiz(headerelements, fragendicts):
     return quiz
 
 def create_question_header(headerelement):
+    """baut header für quiz
+    Parameter: headerelement = String
+    Return: question = ET.Element
+    """
     question=ET.Element('question', type='category')
     question.append(create_category_header(headerelement))
     return question
 
 def create_category_header(headerelement):
+    """baut Kategorie für Quizheader
+    Parameter: headerelement = String
+    Return: category = ET.Element
+    """
     category=ET.Element('category')
     category.append(create_text_header(headerelement))
     return category
 
 def create_text_header(headerelement):
+    """baut text für Kategorieheader
+    Parameter: headerelement = String
+    """
     text = ET.Element('text')
     text.text=headerelement
     return text
 
 def create_question_main(fragendict):
+    """Baut die Frage für XML aus fragendict
+    Parameter: headerelements = List[String], fragendicts = List[Dict]
+        Dict = {'Krankheit':krankheit, 'Umstände':{'Ursachen':[],'Symptome':[],
+                'Komplikationen':[], 'Diagnostiken':[], 'Therapien':[]},'Fragentyp':fragentyp}
+    Return: question = ET.Element
+    """
     question=ET.Element('question', type='multichoice')
     fragentitel=fragendict.get('Fragentitel')
     question.append(create_name_main_kh(fragentitel))
@@ -59,16 +82,27 @@ def create_question_main(fragendict):
     return question
 
 def create_name_main_kh(fragentitel):
+    """baut fragentitel in xml
+    Parameter: fragentitel = String
+    Return: name = ET.Element
+    """
     name=ET.Element('name')
     name.append(create_text_main_kh(fragentitel))
     return name
 
 def create_text_main_kh(fragentitel):
+    """baut fragentitel als text in xml ein
+    Parameter: fragentitel = String
+    Return: text = ET.Element
+    """
     text=ET.Element('text')
     text.text=fragentitel
     return text
 
 def create_feedback_main():
+    """Baut Feedback in xml ein
+    return correctfeedback, partiallycorrectfeedback, incorrectfeedback = ET.Element
+    """
     correctfeedback=ET.Element('correctfeedback', format='html')
     text=ET.Element('text')
     text.text='Die Antwort ist richtig'
@@ -86,6 +120,13 @@ def create_feedback_main():
     return correctfeedback, partiallycorrectfeedback, incorrectfeedback
 
 def create_answer_main(question, fragendict):
+    """
+    Baut Antwort für Frage in xml
+    Parameter: question = ET.Element, fragendict = Dict 
+            Dict = {'Krankheit':krankheit, 'Umstände':{'Ursachen':[],'Symptome':[],
+                'Komplikationen':[], 'Diagnostiken':[], 'Therapien':[]},'Fragentyp':fragentyp}
+    Return: question = ET.Element
+    """
     rcounter=0
     for antwort in fragendict.get('Antworten'):
         if fragendict.get('Antworten').get(antwort)=='right':
@@ -110,6 +151,11 @@ def create_answer_main(question, fragendict):
     return question
 
 def create_file(fragendicts):
+    """Baut XML-File auf für fragen von fragendicts
+    Parameter: fragendicts = List[Dict]
+            Dict = {'Krankheit':krankheit, 'Umstände':{'Ursachen':[],'Symptome':[],
+                'Komplikationen':[], 'Diagnostiken':[], 'Therapien':[]},'Fragentyp':fragentyp}    
+    """
     #header
     dt=datetime.now()
     dtstring=dt.strftime('%Y.%m.%d_%H:%M')
@@ -122,6 +168,5 @@ def create_file(fragendicts):
     exportfile=open('Quizexport.xml', 'wb')
     exportfile.write(data)
     exportfile.close()
-
 
 #export to File
